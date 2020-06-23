772558ebf69d1ff05ce08d6615e9de57a38c8b75
Andrea Galvan <gal.andrea.van@gmail.com>
2020-06-23 10:09:01 +0200

    Update Cpu.kt
    Add fields to Cpu class

--- src/main/kotlin/monkeygb/cpu/Cpu.kt
+++ src/main/kotlin/monkeygb/cpu/Cpu.kt
@@ -1,8 +1,31 @@
 // Cpu.kt
-// Version 1.0
+// Version 1.1
 // Emulates the GameBoy CPU (SHARP LR35902)
 
 package monkeygb.cpu
 
+import monkeygb.memory.MemoryMap
+import monkeygb.registers.Registers
+
 class Cpu {
+    val memoryMap = MemoryMap()
+    val registers = Registers()
+
+    var machineCycles: Long = 0     // number of machine cycles executed; frequency: 1 MHz
+
+    // cpu modes
+    var haltMode: Boolean = false
+    var stopMode: Boolean = false
+    var doubleSpeedMode: Boolean = false
+
+    // instructions implementations are in their own category-specific class
+    val control =  ControlInstructions(this)
+    val jump = JumpCallsInstructions(this)
+    val load8 = LoadInstructions8bit(this)
+    val load16 = LoadInstructions16bit(this)
+    val arithmetic8 = ArithmeticLogical8bit(this)
+    val arithmetic16 = ArithmeticLogical16bit(this)
+    val rotation = RotationShiftBitInstructions(this)
+
+    val opcodes = mutableMapOf<Int, () -> Unit>()   // key: instruction opcode, value: instruction implementation
 }
