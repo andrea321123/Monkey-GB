cdf9c837e0c7d24a41b348acffc76879852dd738
Andrea Galvan <gal.andrea.van@gmail.com>
2020-06-30 10:44:39 +0200

    Update Main.kt
    Add utility functions (getBit() and setBit())

--- src/main/kotlin/monkeygb/Main.kt
+++ src/main/kotlin/monkeygb/Main.kt
@@ -1,5 +1,5 @@
 // Main.kt
-// Version 1.1
+// Version 1.2
 
 package monkeygb
 
@@ -12,4 +12,26 @@ fun complement2toInt(e: Int): Int {
     val negativeE = -(e and 0b10000000)
     val positiveE = e and 0b01111111
     return positiveE + negativeE
+}
+
+// returns bit n of a given number in boolean type (LSB is bit 0)
+fun getBit(number: Int, bit: Int): Boolean {
+    return (number shr bit) and 0x1 == 0x1
+}
+
+// returns a new number that differs from number (param) only for bit n (param) set to set (param)
+fun setBit(number: Int, bit: Int, set: Boolean): Int {
+    var newNumber: Int = number
+    var offset: Int = 1
+
+    offset = offset shl bit
+
+    if (set)
+        newNumber = newNumber or offset
+    else {
+        offset = offset.inv()
+        newNumber = newNumber and offset
+    }
+
+    return newNumber
 }
