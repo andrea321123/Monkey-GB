6d1f2dea339aa7361fa6391eb0ca98ecd3e4a88d
Andrea Galvan <gal.andrea.van@gmail.com>
2020-07-07 12:46:32 +0200

    Add Ppu and Renderer class

--- src/main/kotlin/monkeygb/ppu/Ppu.kt
+++ src/main/kotlin/monkeygb/ppu/Ppu.kt
@@ -1,11 +1,44 @@
 // Ppu.kt
-// Version 1.0
+// Version 1.1
 // Implements the  Game Boy Pixel Processing Unit
 
 package monkeygb.ppu
 
-class Ppu {
+import monkeygb.getBit
+import monkeygb.memory.LCDC
+import monkeygb.memory.MemoryMap
+
+import java.awt.Color
+
+const val RENDER_HEIGHT = 144
+const val RENDER_WIDTH = 160
+
+class Ppu(private val memoryMap: MemoryMap) {
+    // array of colors that represent pixel to render
+    var renderWindow = arrayOf<Array<Color>>()
+
+    init {      // initialize renderWindow
+        for (i in 0 until RENDER_HEIGHT) {
+            var singleLine = arrayOf<Color>()
+            for (j in 0 until RENDER_WIDTH)
+                singleLine += Color.WHITE
+            renderWindow += singleLine
+        }
+    }
+
+    // we draw a single line on the screen
     fun drawScanLine() {
+        val lcdc = memoryMap.getValue(LCDC)
+        if (getBit(lcdc, 0))    // BG enable
+            drawTiles()
+        if (getBit(lcdc, 3))    // sprite enable
+            drawSprites()
+    }
+
+    private fun drawTiles() {
+
+    }
+    private fun drawSprites() {
 
     }
 }
---
+++ src/main/kotlin/monkeygb/ppu/renderer/Renderer.kt
@@ -0,0 +1,49 @@
+// Renderer.kt
+// Version 1.0
+// Render the display from a framebuffer
+
+package monkeygb.ppu.renderer
+
+import monkeygb.memory.MemoryMap
+import monkeygb.ppu.Ppu
+import monkeygb.ppu.RENDER_HEIGHT
+import monkeygb.ppu.RENDER_WIDTH
+import java.awt.Color
+
+import java.awt.Graphics
+import java.awt.image.BufferedImage
+import javax.swing.JFrame
+import javax.swing.JPanel
+
+class Renderer: JPanel() {
+    var image = BufferedImage(RENDER_WIDTH *2, RENDER_HEIGHT *2, BufferedImage.TYPE_INT_RGB)
+    private val frame = JFrame()
+
+    init {
+        // init frame
+        frame.contentPane.add(this)
+        frame.title = "Monkey-GB"
+        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        frame.setSize(RENDER_WIDTH *2, RENDER_HEIGHT *2+22)
+        frame.isVisible = true
+
+        // init panel
+        renderDisplay(Ppu(MemoryMap()).renderWindow)
+    }
+
+    override fun paintComponent(g: Graphics) {
+        g.drawImage(image, 0, 0, this)
+    }
+
+    fun renderDisplay(frameBuffer: Array<Array<Color>>) {
+        for (i in 0 until RENDER_HEIGHT)
+            for (j in 0 until RENDER_WIDTH) {
+                image.setRGB(j *2, i *2, frameBuffer[i][j].rgb)
+                image.setRGB(j *2, i *2 +1, frameBuffer[i][j].rgb)
+                image.setRGB(j *2 +1, i *2, frameBuffer[i][j].rgb)
+                image.setRGB(j *2 +1, i *2 +1, frameBuffer[i][j].rgb)
+            }
+
+        frame.paint(frame.graphics)
+    }
+}
