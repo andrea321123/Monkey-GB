1768e5cd5b75b60012d22934d43a8d7c31984238
andrea321123 <gal.andrea.van@gmail.com>
2020-11-06 21:41:11 +0100

    Update Renderer.kt

--- src/main/kotlin/monkeygb/ppu/renderer/Renderer.kt
+++ src/main/kotlin/monkeygb/ppu/renderer/Renderer.kt
@@ -1,40 +1,42 @@
 // Renderer.kt
-// Version 1.1
+// Version 1.2
 // Render the display from a framebuffer
 
 package monkeygb.ppu.renderer
 
 import monkeygb.joypad.Joypad
-import monkeygb.memory.MemoryMap
-import monkeygb.ppu.Ppu
 import monkeygb.ppu.RENDER_HEIGHT
 import monkeygb.ppu.RENDER_WIDTH
 import java.awt.Color
 
 import java.awt.Graphics
+import java.awt.GridLayout
 import java.awt.image.BufferedImage
-import javax.swing.JFrame
-import javax.swing.JPanel
+import javax.swing.*
 
 class Renderer (private val joypad: Joypad): JPanel() {
-    var image = BufferedImage(RENDER_WIDTH *2, RENDER_HEIGHT *2, BufferedImage.TYPE_INT_RGB)
+    private val image = BufferedImage(RENDER_WIDTH *2, RENDER_HEIGHT *2, BufferedImage.TYPE_INT_RGB)
+    private val imageLabel = JLabel(ImageIcon(image))       // image is encapsulated in a JLabel
     private val frame = JFrame()
 
     init {
+        // init panel
+        layout = GridLayout(1,1)
+        add(imageLabel)
+
         // init frame
+        frame.layout = GridLayout(1,1)
         frame.addKeyListener(joypad)
-        frame.contentPane.add(this)
         frame.title = "Monkey-GB"
         frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-        frame.setSize(RENDER_WIDTH *2, RENDER_HEIGHT *2+22)
         frame.isVisible = true
-
-        // init panel
-        renderDisplay(Ppu(MemoryMap()).renderWindow)
+        frame.isResizable = false
+        frame.add(this)
+        frame.pack()
     }
 
     override fun paintComponent(g: Graphics) {
-        g.drawImage(image, 0, 0, this)
+        imageLabel.paint(g)
     }
 
     fun renderDisplay(frameBuffer: Array<Array<Color>>) {
@@ -46,6 +48,6 @@ class Renderer (private val joypad: Joypad): JPanel() {
                 image.setRGB(j *2 +1, i *2 +1, frameBuffer[i][j].rgb)
             }
 
-        frame.paint(frame.graphics)
+        paint(graphics)
     }
 }
